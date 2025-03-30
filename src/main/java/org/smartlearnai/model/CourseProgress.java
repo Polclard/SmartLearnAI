1f8ec7bca2df13008342a60c3b230d6c94b8c89d
Marija Timova <marija.timova@students.finki.ukim.mk>
2025-03-30 21:26:24 +0200

    UKI67-Create CourseProgress Repository and UIK68-Create CourseProgress Repository

---
+++ src/main/java/org/smartlearnai/model/CourseProgress.java
@@ -0,0 +1,30 @@
+package org.smartlearnai.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CourseProgress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    private int completedLessons;
+
+    private boolean quizPassed;
+}
